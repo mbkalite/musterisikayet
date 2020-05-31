7ef7e99c69a9e377cf226370a8b310066ce51322
musakaplan <musakaplan@musa-air>
2020-05-31 17:23:15 +0300

    Gerekli dependencies'ler eklendi. application properties düzenlendi. Program kullanıma hazır

---
+++ OnlineMusteriSikayet/src/main/java/com/mbkalite/onlinemusterisikayet/OnlineMusteriSikayetApplication.java
@@ -0,0 +1,14 @@
+package com.mbkalite.onlinemusterisikayet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMusteriSikayetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineMusteriSikayetApplication.class, args);
+		System.out.println("hello world");
+	}
+
+}
---
+++ OnlineMusteriSikayet/src/test/java/com/mbkalite/onlinemusterisikayet/OnlineMusteriSikayetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbkalite.onlinemusterisikayet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMusteriSikayetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
